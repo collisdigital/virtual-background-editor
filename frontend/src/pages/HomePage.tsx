e213622fcc15aa659a0edc5878347badcdce76ab
Chris Collis <collisdigital@users.noreply.github.com>
2025-12-09 16:05:34 +0000

    Fix TS6133 error

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import ImageSelector from '../components/ImageSelector';
 import PreviewCanvas from '../components/PreviewCanvas';
 import TextInput from '../components/TextInput';
